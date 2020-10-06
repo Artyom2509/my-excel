268526c9bc94ef635d4c8aaed818286031599a30
Artyom2509 <bicepssapiens228@gmail.com>
2020-10-06 21:02:43 +0600

    finish project config

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const {
+  CleanWebpackPlugin
+} = require( 'clean-webpack-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const path = require( 'path' );
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const jsLoaders = () => {
+  const loaders = [ {
+    loader: "babel-loader",
+    options: {
+      presets: [ '@babel/preset-env' ]
+    }
+  } ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders;
+}
+
+module.exports = {
+  context: path.resolve( __dirname, 'src' ),
+  mode: 'development',
+  entry: [
+    '@babel/polyfill',
+    './index.js'
+  ],
+  devtool: isDev ? 'source-map' : false,
+
+  devServer: {
+    contentBase: path.join( __dirname, 'dist' ),
+    hot: isDev,
+    port: 3000,
+    overlay: true
+  },
+
+  output: {
+    filename: filename( 'js' ),
+    path: path.resolve( __dirname, 'dist' )
+  },
+
+  resolve: {
+    extensions: [ '.js' ],
+    alias: {
+      '@': path.resolve( __dirname, 'src' ),
+      '@core': path.resolve( __dirname, 'src/core' )
+    }
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin( {
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    } ),
+    new CopyWebpackPlugin( {
+      patterns: [ {
+        from: path.resolve( __dirname, 'src/favicon.ico' ),
+        to: path.resolve( __dirname, 'dist' )
+      } ]
+    } ),
+    new MiniCssExtractPlugin( {
+      filename: filename( 'css' )
+    } )
+  ],
+
+  module: {
+    rules: [ {
+        test: /\.s[ac]ss$/i,
+        use: [ {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      }
+    ]
+  }
+}
